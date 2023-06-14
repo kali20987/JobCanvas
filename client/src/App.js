29ff0f90acdf5a291843376ae30ba6abe023fd5b
kali19 <khadijah.syed.ali@gmail.com>
2023-06-14 13:42:46 -0400

    my App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-  import React from 'react';
-  import './App.css';
-  import { ApolloClient, InMemoryCache, ApolloProvider,  } from '@apollo/client';
-  
-  import JobPosting from '../src/pages/JobPosting';
-
-  const client = new ApolloClient({
-    uri: 'http://localhost:3001/graphql',
-    cache: new InMemoryCache()
-  })
+import React from 'react';
+import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider, } from '@apollo/client';
+import JobPosting from '../src/pages/JobPosting';
 
 
-  function App() {
-    return (
-      <ApolloProvider client={client}>
-        <div className="container mx-auto">
-          <JobPosting />
-      </div>
-      </ApolloProvider>
-    );
-  }
+const client = new ApolloClient({
+uri: 'http://localhost:3001/graphql',
+cache: new InMemoryCache()
+})
 
 
-  export default App;
 
+
+function App() {
+return (
+<ApolloProvider client={client}>
+<div className="container mx-auto">
+<JobPosting />
+</div>
+</ApolloProvider>
+);
+}
+export default App;
