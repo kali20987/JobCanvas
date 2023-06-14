281a6dde84f291577046972ea5d95aeb066c2dbd
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-06-14 11:36:31 -0400

    PageContainer made and file structure adjusted.

---
+++ client/src/components/PageContainer.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import NavBar from './NavBar';
+import Home from '../pages/Home'
+import JobPosting from '../pages/JobPosting';
+import SignUp from '../pages/SignUp';
+import LogIn from '../pages/LogIn';
+
+// sets default landing page to home.
+
+export default function PageContainer() {
+    const [currentPage, setCurrentPage] = useState('Home')
+
+
+
+const renderPage = () => {
+
+    if (currentPage === 'Home') {
+        return <Home />;
+
+    }
+    if (currentPage === 'JobPosting') {
+        return <JobPosting />;
+    }
+    if (currentPage === 'Login') {
+        return <LogIn />
+    }
+      return <SignUp />;
+};
+
+const handlePageChange = (page) => setCurrentPage(page);
+
+ return (
+    <div>
+        <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+
+        {renderPage()}
+    </div>
+ );
+};
