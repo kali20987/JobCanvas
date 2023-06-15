cf5111aa7b70cd46e4561d6c570c37d275cac9bc
kali19 <khadijah.syed.ali@gmail.com>
2023-06-15 15:03:12 -0400

    post request

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -32,7 +32,7 @@ type Query {
 type Mutation {
     login(email: String!, password: String!): User
     addUser(firstName: String, lastName: String, gender: String, email: String, password: String): User
-    addJob(companyName: String, location: String, jobTitle: String,  description: String, position: String, salary: String, jobLister: ID
+    addJob(companyName: String, location: String, jobTitle: String,  description: String, position: String, salary: String,
      ): Job
 
 }
