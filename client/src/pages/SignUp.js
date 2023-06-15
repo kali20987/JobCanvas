2b68410421987184e7afbf4260caad6db2cda9cc
Mona Lisa <madison0327@hotmail.com>
2023-06-15 11:49:44 -0400

    signup.js

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,62 +1,121 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { useMuation } from "@apollo/client";
-import { ADD_USER } from "../utils/mutations";
 
-const SignUp = () => {
-  const [formState, setFormState] = useState({
-    firstName: "",
-    lastName: "",
-    gender: "",
-    email: "",
-    password: "",
-  });
-  const [addUser, { error, data }] = useMuation(ADD_USER);
+function Signup() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [gender, setGender] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform signup logic here
+    console.log('Signup submitted:', { firstName, lastName, email, password });
   };
-};
 
-const handleFormSubmit = async (event) => {
-  event.preventDefault();
-  try {
-    const { data } = await addUser({
-      variables: { ...formState },
-    });
-  } catch (e) {
-    console.error(e);
-  }
-};
+  return (
+    <div>
+      <h1>Signup Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Gender:
+          <input type="text" value={gender} onChange={(e) => setGender(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
 
-return (
-  <main className="row g-3 align-items-center">
-    <div className="col-auto">
-      <label for="inputPassword8" className="col-form-label">
-        Password:
-      </label>
-    </div>
-    <div className="col-auto">
-      <input
-        type={"password"}
-        id="inputPassword8"
-        className="form-control"
-        aria-aria-labelledby="passwordHelpInline"
-      >
-        {" "}
-      </input>
-    </div>
-    <div className="col-auto">
-      <span id="passwordHelpInline" className="form-text">
-        {" "}
-        Must be at least 8 characters
-      </span>
-    </div>
-  </main>
-);
+export default Signup;
 
-export default SignUp;
+
+
+// import { useMutation } from "@apollo/client";
+// import { ADD_USER } from "../utils/mutations";
+
+// const SignUp = () => {
+//   const [formState, setFormState] = useState({
+//     firstName: "",
+//     lastName: "",
+//     gender: "",
+//     email: "",
+//     password: "",
+//   });
+//   const [addUser, { error, data }] = useMutation(ADD_USER);
+
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+//     setFormState({
+//       ...formState,
+//       [name]: value,
+//     });
+//   };
+// };
+
+// const handleFormSubmit = async (event) => {
+//   event.preventDefault();
+//   try {
+//     const { data } = await addUser({
+//       variables: { ...formState },
+//     });
+//   } catch (e) {
+//     console.error(e);
+//   }
+// };
+
+// return (
+//   <main className="row g-3 align-items-center">
+//     <h1>SignUp Page</h1>
+//     <form onSubmit={handleFormSubmit}>
+//     <div className="col-auto">
+//         <label >
+//         </label>
+//       </div>
+//       <div className="col-auto">
+//         <label for="inputPassword8" className="col-form-label">
+//           Password:
+//         </label>
+//       </div>
+//       <div className="col-auto">
+//         <input
+//           type={"password"}
+//           id="inputPassword8"
+//           className="form-control"
+//           aria-aria-labelledby="passwordHelpInline"
+//         >
+//           {" "}
+//         </input>
+//       </div>
+//       <div className="col-auto">
+//         <span id="passwordHelpInline" className="form-text">
+//           {" "}
+//           Must be at least 8 characters
+//         </span>
+//       </div>
+//     </form>
+//   </main>
+// );
+
+// export default SignUp;
