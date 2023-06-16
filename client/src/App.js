113c4aa57f2097db03d94862135dcb25be18c7c4
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-06-16 10:54:17 -0400

    Still working on front-end search feature

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import JobPosting from "../src/pages/JobPosting";
+import JobPosting from "./pages/JobPosting";
 import "./App.css";
 import SearchBox from "./components/SearchBox";
 import Home from "./pages/Home";
@@ -50,7 +50,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/jobs" element={<JobPosting />} />
+            <Route path="/job-posting" element={<JobPosting />} />
           </Routes>
         </div>
       </Router>
--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -1,71 +1,72 @@
-import React from 'react';
-import { useState } from 'react'
+import React, { useState } from 'react';
 
+const styles = {
+  jobBoxStyle: { 
+  width:'950px',
+  height:'400px', 
+  lineHeight:'3em',
+  overflow:'scroll', 
+  padding:'5px', 
+  },
+  jobItemStyle: {
+    border: '2px solid',
+    borderRadius: '5px',
+    margin:'0px',
+    padding: '10px'
+  }
+}
 
 export default function SearchBox() {
-
   const [searchInput, setSearchInput] = useState('');
- 
+  const [jobData, setJobData] = useState([]);
 
-  const API_ID = '039bd7f5';
-  const API_KEY = 'e3e8bbf73557a797731aaadbc6f363fb';
-
-  // Updated selector
+  const API_ID = '039bd6f5';
+  const API_KEY = '380d382ccd7b1ed9a74925367df76ea0';
 
   async function jobSearch(input) {
     const data = await fetch(
       `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${API_ID}&app_key=${API_KEY}&results_per_page=50&title_only=${searchInput}`
     );
+//     https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id={YOUR_APP_ID}&app_key={YOUR_APP_KEY}
+
+
+
     const jsonData = await data.json();
-    const jobData = jsonData.results;
+    const jobs = jsonData.results;
+    setJobData(jobs);
+  }
 
-    console.log(jobData);
-
-
-    for (let x = 0; x < jobData.length; x++) { 
-      console.log(` 
-        ${jobData[x].company.display_name},
-        ${jobData[x].title},
-        ${jobData[x].description},
-        ${jobData[x].location.display_name},
-        ${jobData[x].contract_type},
-        ${jobData[x].salary_max}
-      `);
-      const Company = jobData[x].company.display_name;
-      const Title = jobData[x].title;
-      const Description = jobData[x].description;
-      const Location = jobData[x].location.display_name;
-      const Position = jobData[x].contract_type;
-      const Salary = jobData[x].salary;
-return `
-${Company},
-${Title},
-${Description},
-${Location},
-${Position},
-${Salary},
-    `;
-
-    }
-  };
   return (
     <div className="search-container">
-
       <input
-        id="input" type="text" className="input" placeholder="Search..."
+        id="input"
+        type="text"
+        className="input"
+        placeholder="Search..."
         onChange={(event) => setSearchInput(event.target.value)}
       />
-      <button id="searchBtn" className="searchBtn" onClick={jobSearch} >Search</button>
+      <button id="searchBtn" className="searchBtn" onClick={jobSearch}>
+        Search
+      </button>
 
-      <div>
-          <div>
-            <h3>HELLO</h3>
+      <div style={styles.jobBoxStyle}  >
+        <h3>Search Result:</h3>
+        {jobData.map((job, index) => (
+
+          <div className="job-item" key={index}  >
+
+            <h5>{job.company.display_name}</h5>
+            <p>{job.title}</p>
+            <p>{job.description}</p>
+            <p>{job.location.display_name}</p>
+            <p>{job.contract_type}</p>
+            <p>{job.salary}</p>
+
           </div>
-       
+
+
+        ))}
       </div>
-
     </div>
-  )
-
-
-};
+  );
+}
