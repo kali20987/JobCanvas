7f551c3af9cea856d4bdf6e30788c4f1cc2b6840
Daisha <daishadeniz@gmail.com>
2023-06-16 20:44:51 -0400

    added footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container">
+        <a className="navbar-brand text-white" href="#">
+        ©2023 Job Canvas. All rights reserved. 
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
